d7e28be932bc9d995ba5b908685260c5dfefb2cd
Lucas Pacheco <lucaspsilveira@live.com>
2019-06-05 22:36:13 -0300

    Teste de registro de dados do usuário

---
+++ tests/Feature/UsuarioRegistroDadosTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\UsuarioGlicoseRegistro;
+
+class UsuarioRegistroDadosTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     *
+     * @test
+     */
+    public function um_registro_glicose_pode_ser_adicionado()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/UsuarioGlicoseRegistros', [
+            'id_usuario' => 1,
+            'glicose' => 20,
+        ]);
+        $response->assertOk();
+        $this->assertCount(1, UsuarioGlicoseRegistro::all());
+    }
+}
