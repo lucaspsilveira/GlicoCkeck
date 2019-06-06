906f1a966262078636a5776093acfe9fca95cd41
Lucas Pacheco <lucaspsilveira@live.com>
2019-06-05 23:33:37 -0300

    Testes de registro de altura

--- tests/Feature/UsuarioRegistroDadosTest.php
+++ tests/Feature/UsuarioRegistroDadosTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\UsuarioGlicoseRegistro;
+use App\UsuarioAlturaRegistro;
 
 class UsuarioRegistroDadosTest extends TestCase
 {
@@ -35,6 +36,37 @@ class UsuarioRegistroDadosTest extends TestCase
         $response = $this->post('/UsuarioGlicoseRegistros', [
             'id_usuario' => '1',
             'glicose' => '1',
+        ]);
+        $response->assertSessionHasErrors();
+    }
+    /**
+     * A basic test example.
+     *
+     * @test
+     */
+    public function um_registro_altura_pode_ser_adicionado()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/UsuarioAlturaRegistros', [
+            'id_usuario' => 1,
+            'altura' => 1.8,
+            'unidade' => 'metros',
+        ]);
+        $response->assertOk();
+        $this->assertCount(1, UsuarioAlturaRegistro::all());
+    }
+    /**
+     * A basic test example.
+     *
+     * @test
+     */
+    public function um_registro_altura_tem_que_ter_altura_e_usuario_e_unidade()
+    {
+        //$this->withoutExceptionHandling();
+        $response = $this->post('/UsuarioAlturaRegistros', [
+            'id_usuario' => '1',
+            'altura' => '1',
+            'unidate' => 'metros',
         ]);
         $response->assertSessionHasErrors();
     }
