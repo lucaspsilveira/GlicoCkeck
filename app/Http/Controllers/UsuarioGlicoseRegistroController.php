16fb39a073c6d467b6dda11fcfc5ec8e2a6e4103
Lucas Pacheco <lucaspsilveira@live.com>
2019-06-05 22:37:01 -0300

    Adicionar Registro de glicose

--- app/Http/Controllers/UsuarioGlicoseRegistroController.php
+++ app/Http/Controllers/UsuarioGlicoseRegistroController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\UsuarioGlicoseRegistro;
 
 class UsuarioGlicoseRegistroController extends Controller
 {
-    //
+    public function store() {
+        UsuarioGlicoseRegistro::create([
+            'id_usuario' => request('id_usuario'),
+            'glicose' => request('glicose'),
+        ]);
+    }
 }
