d068d7f0e0e6a2b183f7864a2986af1fa3c961ce
Lucas Pacheco <lucaspsilveira@live.com>
2019-06-06 22:57:37 -0300

    Ajuste unidade

--- resources/views/usuarioGlicoseRegistro/index.blade.php
+++ resources/views/usuarioGlicoseRegistro/index.blade.php
@@ -5,7 +5,7 @@
     <h1>Registros de Glicose</h1>
     <ul>
         @foreach ($registrosGlicose as $registro)
-            <li>{{$registro->created_at}}: {{$registro->glicose}}</li>
+            <li>{{$registro->created_at}}: {{$registro->glicose}} mg/dl</li>
         @endforeach
     </ul>
     
