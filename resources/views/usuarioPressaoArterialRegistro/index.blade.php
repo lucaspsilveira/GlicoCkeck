5cb519fe0484f9c791e84b3679f07f9dfd35787f
Lucas Pacheco <lucaspsilveira@live.com>
2019-06-10 14:31:12 -0300

    Gráfico Pressão arterial

--- resources/views/usuarioPressaoArterialRegistro/index.blade.php
+++ resources/views/usuarioPressaoArterialRegistro/index.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
-
+@push('head')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>    
+@endpush
 @section('content')
 
     <h1 class="title">Registros de Pressão Arterial</h1>
+    <canvas id="myChart" width="100%" height=""></canvas>
         <table class="table is-fullwidth">
                 <thead>
                     <th class="">Data/Hora Registro</th>
@@ -10,8 +14,14 @@
                     <th class="">Ações</th>
                 </thead>
                 <tbody>
+                    <?php $datas = array(); $glicose = array(); ?>
                 @foreach ($registrosPressaoArterial as $registro)
                     <tr>
+                        <?php
+                        $datas[] = $registro->created_at->format("d/m/Y");
+                        $pressao_sistolica[] = $registro->pressao_arterial_sistolica;
+                        $pressao_diastolica[] = $registro->pressao_arterial_diastolica;
+                        ?>
                         <th>{{$registro->created_at->format("d/m/Y H:i:s")}}</th> 
                         <td>{{$registro->pressao_arterial_sistolica . "/". $registro->pressao_arterial_diastolica  }} mg/dl</td>
                         <td>                          
@@ -58,6 +68,48 @@
                             element.classList.remove("is-active"); 
                             element.classList.remove("is-clipped");
                         }
+                        var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          
+          labels: <?php  echo json_encode(array_reverse($datas));?>,
+            datasets: [{
+                label: 'pressão sistólica',
+                data: <?php  echo json_encode(array_reverse($pressao_sistolica)); ?>,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1,
+                fill: false
+            },
+            {
+                label: 'pressão diastólica',
+                data: <?php  echo json_encode(array_reverse($pressao_diastolica)); ?>,
+                backgroundColor: [
+                    'rgba(215, 99, 0, 1)',
+                ],
+                borderColor: [
+                    'rgba(215, 99, 0, 1)',
+                ],
+                borderWidth: 1,
+                fill: false
+                
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
                     </script>  
     
 @endsection
