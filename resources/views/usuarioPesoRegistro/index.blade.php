3736374bec8db38b246356b9394a71f16d477533
Lucas Pacheco <lucaspsilveira@live.com>
2019-06-10 00:06:28 -0300

    Adicionada confirmação de exclusão

--- resources/views/usuarioPesoRegistro/index.blade.php
+++ resources/views/usuarioPesoRegistro/index.blade.php
@@ -14,13 +14,9 @@
         <tr>
                 <th>{{$registro->created_at->format("d/m/Y H:i:s")}}</th> 
                 <td>{{(str_replace('.', ',',$registro->peso))." ". $registro->unidade}}</td>
-                <td>
-                        <form action="/UsuarioPesoRegistros/{{$registro->id}}" method="POST">
-                                @method('DELETE')
-                                @csrf
-                                
-                                <button class="delete" type="submit"></button>              
-                               </form>
+                <td>                            
+                            <button class="delete" type="submit" onclick="ativaModal({{$registro->id}})"></button>              
+                               
                             <span class="icon "onclick="location.href='/UsuarioPesoRegistros/{{$registro->id}}/edit'">
                                 <i class="fas fa-edit"></i>
                               </span>
@@ -28,5 +24,39 @@
         </tr>
         @endforeach
             </tbody>
-    
+    </table>
+            <div class="modal" id="modal-verificacao">
+                    <div class="modal-background"></div>
+                    <div class="modal-card">
+                      <header class="modal-card-head">
+                        <p class="modal-card-title">Confirmação de exclusão</p>
+                        <button class="delete" aria-label="close" onclick="desativaModal()"></button>
+                      </header>
+                      <section class="modal-card-body">
+                        <p>Você realmente deseja excluir esse registro? Essa ação é irreversível.</p>
+                      </section>
+                      <footer class="modal-card-foot">
+                        <form id="form-exclusao" action="" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            
+                            <button class="button is-danger" type="submit">Excluir</button>              
+                           </form>
+                        <button class="button" onclick="desativaModal()">Cancel</button>
+                      </footer>
+                    </div>
+                  </div>
+                    <script>
+                            function ativaModal(id) {
+                                var element = document.getElementById("modal-verificacao");
+                                document.getElementById("form-exclusao").action = "/UsuarioPesoRegistros/"+ id;
+                                element.classList.add("is-active"); 
+                                element.classList.add("is-clipped");
+                            }
+                            function desativaModal() {
+                                var element = document.getElementById("modal-verificacao");
+                                element.classList.remove("is-active"); 
+                                element.classList.remove("is-clipped");
+                            }
+                        </script>   
 @endsection
