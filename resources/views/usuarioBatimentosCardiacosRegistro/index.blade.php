03fa21ffbfb6231827efcdb5781ae7bfec799889
Lucas Pacheco <lucaspsilveira@live.com>
2019-06-10 14:18:11 -0300

    Gráfico batimentos cardíacos

--- resources/views/usuarioBatimentosCardiacosRegistro/index.blade.php
+++ resources/views/usuarioBatimentosCardiacosRegistro/index.blade.php
@@ -1,17 +1,27 @@
 @extends('layouts.app')
-
+@push('head')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>    
+@endpush
 @section('content')
 
     <h1 class="title">Registros de batimentos cardíacos</h1>
+    <canvas id="myChart" width="100%" height=""></canvas>
     <table class="table is-fullwidth">
             <thead>
                 <th class="">Data/Hora Registro</th>
-                <th class="">Glicose</th>
+                <th class="">Batimentos Cardíacos</th>
                 <th class="">Ações</th>
             </thead>
+            <?php $datas = array(); $glicose = array(); ?>
+            
             <tbody>
                 @foreach ($registrosBatimentosCardiacos as $registro)
                 <tr>
+                    <?php
+                    $datas[] = $registro->created_at->format("d/m/Y");
+                    $batimentos_cardiacos[] = $registro->batimentos_cardiacos;
+                    ?>
                         <th>{{$registro->created_at->format("d/m/Y H:i:s")}}</th> 
                         <td>{{(str_replace('.', ',',$registro->batimentos_cardiacos))}} bpm</td>
                         <td>
@@ -58,5 +68,34 @@
                         element.classList.remove("is-active"); 
                         element.classList.remove("is-clipped");
                     }
+                    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          
+          labels: <?php  echo json_encode(array_reverse($datas));?>,
+            datasets: [{
+                label: 'batimentos cardíacos',
+                data: <?php  echo json_encode(array_reverse($batimentos_cardiacos)); ?>,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1,
+                lineTension:0.1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
                 </script>  
 @endsection
